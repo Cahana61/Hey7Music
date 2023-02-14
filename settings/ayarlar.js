ca6dbdd07574f0eb77eee9725ab4eec2f53275cd
Cahana61 <93998539+Cahana61@users.noreply.github.com>
2023-02-14 15:22:17 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTAxNTY5Mzg5MTk3MTg2MjY0MA.GMTerO.m24Exx7wMOHa_2n_NHEatFrSQLFYrbaieYtnW4",
+    TOKEN: process.env.TOKEN || "",
     PREFIX: process.env.PREFIX || "!",
-    SAHİP: process.env.SAHİP || "916677878744223764",
+    SAHİP: process.env.SAHİP || "",
 }
